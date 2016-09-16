a32aa44ff4eb4dda45afadbca6b34de11239db76
Marc Lindenbach <marc.lindenbach@gmail.com>
2016-09-16 15:59:10 -0600

    added most of the chat features

--- server.py
+++ server.py
@@ -13,14 +13,21 @@ cache = SimpleCache()
 def emit_user_list():
     emit('user_list', cache.get('user_list'), broadcast=True)
 
-def emit_chat_message():
-    pass
+def emit_chat_message(user_name, chat_message):
+    emit('chat_message', {'chat_message': chat_message, 'user_name': user_name}, broadcast=True)
 
 @app.route('/')
 def index():
     if 'uid' not in session:
         session['uid'] = str(uuid.uuid4())
-    return render_template('index.html')
+
+    user_list = cache.get('user_list')
+    if user_list is None or not session['uid'] in user_list:
+        user_name = ''
+    else:
+        user_name = user_list[session['uid']]['name']
+
+    return render_template('index.html', user_name=user_name)
 
 @socketio.on('connect')
 def on_connect():
@@ -57,8 +64,23 @@ def on_disconnect():
     emit_user_list()
 
 @socketio.on('message')
-def on_message(event_name, user_name):
-    pass
+def on_message(event_name, message_text):
+    user_list = cache.get('user_list')
+
+    if not session['uid'] in user_list:
+        user_name = ''
+    else:
+        user_name = user_list[session['uid']]['name']
+
+    if event_name == 'send_chat_message':
+        emit_chat_message(user_name, message_text)
+        return
+
+    if event_name == 'change_user_name':
+        user_list[session['uid']]['name'] = message_text
+        cache.set('user_list', user_list)
+        emit_user_list()
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
